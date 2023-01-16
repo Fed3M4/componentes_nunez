a71aafc7c6c73a2bd0b2c619880b5d92d5a9c8c4
Federico <nunez.fedema@gmmail.com>
2023-01-16 15:28:58 -0300

    Preparación para la primera entrega

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { AsideComponent } from './componentes/aside/aside.component';
+import { MainComponent } from './componentes/main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AsideComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule
